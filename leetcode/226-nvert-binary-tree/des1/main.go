680af83c01c328f62b04cfd66522233106c4882a
Lilit Smotrova <l.smotrova@omp.ru>
2023-09-23 04:08:31 +0400

    226. Invert Binary Tree
    See https://leetcode.com/problems/invert-binary-tree/

---
+++ leetcode/226-nvert-binary-tree/des1/main.go
@@ -0,0 +1,59 @@
+package des1
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	stack := stack{}
+	stack.push(root)
+	for !stack.isEmpty() {
+		n := stack.pop()
+		if n == nil {
+			continue
+		}
+		n.Left, n.Right = n.Right, n.Left
+		stack.push(n.Left)
+		stack.push(n.Right)
+	}
+	return root
+}
+
+type nodeStack struct {
+	val  *TreeNode
+	next *nodeStack
+}
+
+type stack struct {
+	top *nodeStack
+}
+
+func (s *stack) push(val *TreeNode) {
+	s.top = &nodeStack{
+		val:  val,
+		next: s.top,
+	}
+}
+
+func (s *stack) pop() *TreeNode {
+	if s.isEmpty() {
+		panic("stack is empty")
+	}
+	n := s.top
+	s.top = n.next
+	return n.val
+}
+
+func (s *stack) isEmpty() bool {
+	return s.top == nil
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
---
+++ leetcode/226-nvert-binary-tree/des2/main.go
@@ -0,0 +1,29 @@
+package des1
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	invert(root)
+	return root
+}
+
+func invert(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invert(root.Left)
+	invert(root.Right)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
