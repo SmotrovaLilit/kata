b86a950d6e5953ca2b3ab28e77f301288e3b2806
Lilit Smotrova <l.smotrova@omp.ru>
2023-09-23 04:27:46 +0400

    101. Symmetric Tree
    See https://leetcode.com/problems/symmetric-tree/

---
+++ leetcode/101-symmetric-tree/des2/main_test.go
@@ -0,0 +1,72 @@
+package des1
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_isSymmetric(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "t1",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 3,
+						},
+						Right: &TreeNode{
+							Val: 4,
+						},
+					},
+					Right: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 4,
+						},
+						Right: &TreeNode{
+							Val: 3,
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "t2",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val: 2,
+						Right: &TreeNode{
+							Val: 3,
+						},
+					},
+					Right: &TreeNode{
+						Val: 2,
+						Right: &TreeNode{
+							Val: 3,
+						},
+					},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := isSymmetric(tt.args.root)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
