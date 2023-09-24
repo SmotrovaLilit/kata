7de7098826b04eb84f830b123f24e7e88a3b1d92
Lilit Smotrova <l.smotrova@omp.ru>
2023-09-24 16:49:44 +0400

    530. Minimum Absolute Difference in BST
    See https://leetcode.com/problems/minimum-absolute-difference-in-bst

---
+++ leetcode/530-minimum-absolute-difference-in-bst/des1/main.go
@@ -0,0 +1,35 @@
+package des0
+
+import (
+	. "leetcode/leetcode/pkg/tree"
+	"math"
+	"sort"
+)
+
+func getMinimumDifference(root *TreeNode) int {
+	values := make([]int, 0)
+	dfs(root, &values)
+	sort.Ints(values)
+	minValue := math.MaxInt
+	for i := 1; i < len(values); i++ {
+		minValue = min(minValue, distance(values[i], values[i-1]))
+	}
+	return minValue
+}
+
+func dfs(root *TreeNode, values *[]int) {
+	if root == nil {
+		return
+	}
+	dfs(root.Left, values)
+	*values = append(*values, root.Val)
+	dfs(root.Right, values)
+}
+
+func distance(a, b int) int {
+	dist := a - b
+	if dist < 0 {
+		dist *= -1
+	}
+	return dist
+}
